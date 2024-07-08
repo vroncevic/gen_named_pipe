ecda7aede1bc9d893c0f8375078bd0c777861051
Vladimir <elektron.ronca@gmail.com>
2024-07-09 01:13:20 +0200

    [gen_named_pipe] Updated ATS CLI mechanism, docs

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -7,6 +7,12 @@
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
 #
 
+rm -rf htmlcov gen_named_pipe_coverage.xml gen_named_pipe_coverage.json .coverage
+rm -rf fresh_new/ full_simple_new/ latest_pro/ simple_read/ simple_write/
+ats_coverage_run.py -n gen_named_pipe -p ../README.md
 rm -rf fresh_new/ full_simple_new/ latest_pro/ simple_read/ simple_write/
 python3 -m coverage run -m --source=../gen_named_pipe unittest discover -s ./ -p '*_test.py' -vvv
-python3 -m coverage html
+python3 -m coverage html -d htmlcov
+python3 -m coverage xml -o gen_named_pipe_coverage.xml 
+python3 -m coverage json -o gen_named_pipe_coverage.json
+python3 -m coverage report --format=markdown -m
